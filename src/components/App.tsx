03367fdd51adf2c7ab448ef2f4c774b4ed88f145
Danny O'Brien <danmcfan33@gmail.com>
2025-11-03 16:43:00 -0800

    a board

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,15 @@
 import { onCleanup, onMount, createSignal, type JSX } from "solid-js";
-import {
-  Hammer,
-  FastForward,
-  PersonStanding,
-  StretchHorizontal,
-  Landmark,
-  Settings,
-} from "lucide-solid";
+import { Hammer, FastForward, Landmark, Settings } from "lucide-solid";
 import { Dice } from "@/components/Dice";
 import { cn } from "@/lib/utils";
+import { circle, hexagon, text } from "@/lib/draw";
 
 const FPS = 60;
 const FRAME_TIME = 1000 / FPS;
 
 const SCROLL_SPEED = 5;
+
+const RADIUS = 100;
 
 type CardType =
   | "lumber"
@@ -24,7 +20,38 @@ type CardType =
   | "resource"
   | "development";
 
-type Color = "red" | "blue" | "green" | "yellow";
+type TileType = "desert" | "lumber" | "brick" | "wool" | "grain" | "ore";
+
+type Tile = {
+  type: TileType;
+  value: number;
+};
+
+const COLORS = {
+  DesertTop: "oklch(90.1% 0.076 70.697)",
+  DesertBottom: "oklch(83.7% 0.128 66.29)",
+  DesertBorder: "oklch(70.5% 0.213 47.604)",
+  LumberTop: "oklch(52.7% 0.154 150.069)",
+  LumberBottom: "oklch(44.8% 0.119 151.328)",
+  LumberBorder: "oklch(39.3% 0.095 152.535)",
+  BrickTop: "oklch(64.6% 0.222 41.116)",
+  BrickBottom: "oklch(55.3% 0.195 38.402)",
+  BrickBorder: "oklch(40.8% 0.123 38.172)",
+  WoolTop: "oklch(72.3% 0.219 149.579)",
+  WoolBottom: "oklch(62.7% 0.194 149.214)",
+  WoolBorder: "oklch(44.8% 0.119 151.328)",
+  GrainTop: "oklch(79.5% 0.184 86.047)",
+  GrainBottom: "oklch(68.1% 0.162 75.834)",
+  GrainBorder: "oklch(47.6% 0.114 61.907)",
+  OreTop: "oklch(70.9% 0.01 56.259)",
+  OreBottom: "oklch(55.3% 0.013 58.071)",
+  OreBorder: "oklch(37.4% 0.01 67.558)",
+  ChitTop: "oklch(98.5% 0 0)",
+  ChitBottom: "oklch(92% 0.004 286.32)",
+  ChitBorder: "oklch(70.5% 0.015 286.067)",
+};
+
+type Color = (typeof COLORS)[keyof typeof COLORS];
 
 export function App() {
   let parentRef: HTMLDivElement | undefined;
@@ -34,12 +61,102 @@ export function App() {
   let lastTimestamp = 0;
   let lag = 0;
 
-  let color = "red";
-  let frames = 0;
-  let framesMax = 60;
-
   let x = 0;
   let y = 0;
+
+  let tiles: Tile[][] = [
+    // Row 1 (3 tiles)
+    [
+      {
+        type: "ore",
+        value: 10,
+      },
+      {
+        type: "wool",
+        value: 2,
+      },
+      {
+        type: "lumber",
+        value: 9,
+      },
+    ],
+    // Row 2 (4 tiles)
+    [
+      {
+        type: "grain",
+        value: 12,
+      },
+      {
+        type: "brick",
+        value: 6,
+      },
+      {
+        type: "wool",
+        value: 4,
+      },
+      {
+        type: "brick",
+        value: 10,
+      },
+    ],
+    // Row 3 (5 tiles)
+    [
+      {
+        type: "grain",
+        value: 9,
+      },
+      {
+        type: "lumber",
+        value: 11,
+      },
+      {
+        type: "desert",
+        value: 0,
+      },
+      {
+        type: "lumber",
+        value: 3,
+      },
+      {
+        type: "ore",
+        value: 8,
+      },
+    ],
+    // Row 4 (4 tiles)
+    [
+      {
+        type: "lumber",
+        value: 8,
+      },
+      {
+        type: "ore",
+        value: 3,
+      },
+      {
+        type: "grain",
+        value: 4,
+      },
+      {
+        type: "wool",
+        value: 5,
+      },
+    ],
+    // Row 5 (3 tiles)
+    [
+      {
+        type: "brick",
+        value: 5,
+      },
+      {
+        type: "grain",
+        value: 6,
+      },
+      {
+        type: "wool",
+        value: 11,
+      },
+    ],
+  ];
 
   const keys = new Set<string>();
 
@@ -57,22 +174,16 @@ export function App() {
 
   function update() {
     if (keys.has("ArrowUp") || keys.has("KeyW")) {
-      y -= SCROLL_SPEED;
-    }
-    if (keys.has("ArrowDown") || keys.has("KeyS")) {
       y += SCROLL_SPEED;
     }
-    if (keys.has("ArrowLeft") || keys.has("KeyA")) {
-      x -= SCROLL_SPEED;
+    if (keys.has("ArrowDown") || keys.has("KeyS")) {
+      y -= SCROLL_SPEED;
     }
-    if (keys.has("ArrowRight") || keys.has("KeyD")) {
+    if (keys.has("ArrowLeft") || keys.has("KeyA")) {
       x += SCROLL_SPEED;
     }
-
-    frames++;
-    if (frames >= framesMax) {
-      frames = 0;
-      color = color === "red" ? "blue" : "red";
+    if (keys.has("ArrowRight") || keys.has("KeyD")) {
+      x -= SCROLL_SPEED;
     }
   }
 
@@ -85,8 +196,88 @@ export function App() {
     ctx.save();
 
     ctx.translate(x, y);
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, 100, 100);
+
+    const startX = -RADIUS * 2;
+    const startY = -RADIUS * 1.75 * 2;
+
+    let rowIndex = 0;
+    let tileX = startX;
+    let tileY = startY;
+    for (const row of tiles) {
+      for (const tile of row) {
+        let topColor = "";
+        let bottomColor = "";
+        let borderColor = "black";
+        switch (tile.type) {
+          case "desert":
+            topColor = COLORS.DesertTop;
+            bottomColor = COLORS.DesertBottom;
+            borderColor = COLORS.DesertBorder;
+            break;
+          case "lumber":
+            topColor = COLORS.LumberTop;
+            bottomColor = COLORS.LumberBottom;
+            borderColor = COLORS.LumberBorder;
+            break;
+          case "brick":
+            topColor = COLORS.BrickTop;
+            bottomColor = COLORS.BrickBottom;
+            borderColor = COLORS.BrickBorder;
+            break;
+          case "wool":
+            topColor = COLORS.WoolTop;
+            bottomColor = COLORS.WoolBottom;
+            borderColor = COLORS.WoolBorder;
+            break;
+          case "grain":
+            topColor = COLORS.GrainTop;
+            bottomColor = COLORS.GrainBottom;
+            borderColor = COLORS.GrainBorder;
+            break;
+          case "ore":
+            topColor = COLORS.OreTop;
+            bottomColor = COLORS.OreBottom;
+            borderColor = COLORS.OreBorder;
+            break;
+          default:
+            throw new Error(`Unknown tile type: ${tile.type}.`);
+        }
+        hexagon(ctx, tileX, tileY, RADIUS, topColor, bottomColor, borderColor);
+        if (tile.value > 0) {
+          let color = "black";
+          if (tile.value === 6 || tile.value === 8) {
+            color = "oklch(57.7% 0.245 27.325)";
+          }
+
+          circle(
+            ctx,
+            tileX,
+            tileY,
+            RADIUS * 0.35,
+            COLORS.ChitTop,
+            COLORS.ChitBottom,
+            color,
+          );
+
+          text(ctx, tileX, tileY + 4, tile.value.toString(), color);
+        }
+        tileX += RADIUS * 2;
+      }
+      rowIndex++;
+      switch (rowIndex) {
+        case 1:
+        case 3:
+          tileX = startX - RADIUS;
+          break;
+        case 2:
+          tileX = startX - RADIUS * 2;
+          break;
+        default:
+          tileX = startX;
+          break;
+      }
+      tileY += RADIUS * 1.75;
+    }
 
     ctx.restore();
   }
@@ -98,8 +289,8 @@ export function App() {
     canvasRef.width = parentRef.clientWidth;
     canvasRef.height = parentRef.clientHeight;
 
-    x = canvasRef.width / 2 - 50;
-    y = canvasRef.height / 2 - 50;
+    x = canvasRef.width / 2;
+    y = canvasRef.height / 2;
 
     ctx = canvasRef.getContext("2d");
   }
@@ -295,8 +486,8 @@ function Card(props: { type: CardType }) {
     brick: "from-orange-600 to-orange-700",
     wool: "from-green-500 to-green-600",
     grain: "from-yellow-500 to-yellow-600",
-    ore: "from-zinc-400 to-zinc-500",
-    resource: "from-zinc-400 to-zinc-500",
+    ore: "from-stone-400 to-stone-500",
+    resource: "from-blue-400 to-blue-500",
     development: "from-purple-400 to-purple-500",
   };
 
---
+++ src/lib/draw.ts
@@ -0,0 +1,78 @@
+export function hexagon(
+  ctx: CanvasRenderingContext2D,
+  cx: number,
+  cy: number,
+  r: number,
+  topColor: string,
+  bottomColor: string,
+  borderColor: string = "black",
+  borderWidth: number = 4,
+) {
+  ctx.save();
+
+  ctx.beginPath();
+  for (let i = 0; i < 6; i++) {
+    const angle = -Math.PI / 2 + (i * Math.PI) / 3;
+    const x = cx + r * Math.cos(angle);
+    const y = cy + r * Math.sin(angle);
+    if (i === 0) ctx.moveTo(x, y);
+    else ctx.lineTo(x, y);
+  }
+  ctx.closePath();
+
+  const grad = ctx.createLinearGradient(cx, cy - r, cx, cy + r);
+  grad.addColorStop(0, topColor);
+  grad.addColorStop(1, bottomColor);
+  ctx.fillStyle = grad;
+  ctx.fill();
+
+  ctx.strokeStyle = borderColor;
+  ctx.lineWidth = borderWidth;
+  ctx.stroke();
+
+  ctx.restore();
+}
+
+export function circle(
+  ctx: CanvasRenderingContext2D,
+  cx: number,
+  cy: number,
+  r: number,
+  topColor: string,
+  bottomColor: string,
+  borderColor: string = "black",
+  borderWidth: number = 2,
+) {
+  ctx.save();
+
+  ctx.beginPath();
+  ctx.arc(cx, cy, r, 0, 2 * Math.PI);
+  ctx.closePath();
+
+  const grad = ctx.createLinearGradient(cx, cy - r, cx, cy + r);
+  grad.addColorStop(0, topColor);
+  grad.addColorStop(1, bottomColor);
+  ctx.fillStyle = grad;
+  ctx.fill();
+
+  ctx.strokeStyle = borderColor;
+  ctx.lineWidth = borderWidth;
+  ctx.stroke();
+
+  ctx.restore();
+}
+
+export function text(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  text: string,
+  color: string = "black",
+  font: string = "32px monospace",
+) {
+  ctx.fillStyle = color;
+  ctx.font = font;
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText(text, x, y);
+}
